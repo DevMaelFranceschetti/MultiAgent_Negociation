2dbf1e2ef8df0ff385e5a5d47c62f1b6f9020f46
Ouaguenouni Mohamed <ouaguenouni.hachemi@gmail.com>
2021-02-21 20:10:28 +0100

    Generalized Regret Matching with fadding memory and intertia

--- core/Player.py
+++ core/Player.py
@@ -3,6 +3,9 @@ from Environnement import *
 import threading
 import time
 import logging
+from Functions import *
+import random
+
 
 logging.basicConfig(level=logging.DEBUG,
                     format='[%(levelname)s] (%(threadName)-10s) %(message)s',
@@ -70,8 +73,6 @@ class BRPlayer(Player):
 
 
 class FPPlayer1(Player):
-        
-    
     def select_action(self):
         return np.argmax(self.env.getExpectationPlayer(self.id))
 
@@ -97,13 +98,12 @@ class FPPlayer(Player):
         return np.argmax(a)
 
 
-class RMPlayer(Player):
+class RegretMatchingPlayer(Player):
 
     def __init__(self, environnement, id):
         super().__init__( environnement, id)
         self.cumulated_reget = np.zeros(self.env.tasks.shape)
         self.step = 0
-        pass
 
     def observe(self):
         pred_ac = self.env.getPrecedentActionsMatrix().T
@@ -117,6 +117,95 @@ class RMPlayer(Player):
         u = self.env.utility[moves_v]
         return np.max(u) - u[actions[self.id]]
     
+    def select_action(self):
+        norm_regret = (self.cumulated_reget / self.step) if (self.step != 0 ) else self.cumulated_reget
+        return np.argmin(norm_regret)
+
+
+
+class SAPlayer(Player):
     
     def select_action(self):
-        return np.argmin(self.cumulated_reget)
+        ch = self.env.getUnilateralChanges(self.id)[:,self.id]
+        ch = softmax(ch)
+        act = np.random.choice(self.env.tasks, p=ch)
+        return self.env.getBestAction(self.id)[1]
+    
+
+
+
+
+
+class SpatialFictiousPlayPlayer(Player):
+
+    def __init__(self, environnement, id, func = softmax):
+        super().__init__( environnement, id)
+        self.func = func
+
+    def getExpectedUtility(self):
+        fmatrix = self.env.getFrequencyMatrixL()
+        t = len(self.env.players)*[slice(None, None, None)]+[self.id]
+        it = np.nditer(self.env.utility[t], flags=['multi_index'])
+        act = np.zeros(self.env.tasks.shape)
+        for x in it:
+            p = 1
+            for n,a in  enumerate(it.multi_index):
+                p = p  * (fmatrix[n][a])
+            mya = it.multi_index[self.id]
+            act[mya] = act[mya] + p*x
+        return act
+        
+    
+    def select_action(self):
+        a = self.getExpectedUtility()
+        ch = self.func(a)
+        act = np.random.choice(self.env.tasks, p=ch)
+        return act
+
+
+
+class GeneralizedRegretMatchingPlayer(Player):
+    
+    def __init__(self, environnement, id, func = softmax, fadding_factor = 0, inertia_factor = 0):
+        super().__init__( environnement, id)
+        self.cumulated_reget = np.zeros(self.env.tasks.shape)
+        self.precedent_action = random.randint(0, len(self.env.tasks))
+        self.step = 0
+        self.func = func
+        self.fadding_factor = fadding_factor
+        self.inertia_factor = inertia_factor
+
+    def observe(self):
+        pred_ac = self.env.getPrecedentActionsMatrix().T
+        #self.cumulated_reget = self.cumulated_reget*(1-self.fadding_factor)
+        for i in range(self.step, pred_ac.shape[0]):
+            act = pred_ac[i]
+            self.cumulated_reget[act[self.id]] = self.cumulated_reget[act[self.id]] + self.getRegret(act)
+        self.step = pred_ac.shape[0]
+
+    def getRegret(self, actions):
+        moves_v = list(actions[:self.id]) + [slice(None, None, None)] + list(actions[self.id+1:])+ [self.id]
+        u = self.env.utility[moves_v]
+        return np.max(u) - u[actions[self.id]]
+    
+    def select_action(self):
+        norm_regret = (self.cumulated_reget / self.step) if (self.step != 0 ) else self.cumulated_reget
+        norm_regret = norm_regret.max()-norm_regret
+        ch = self.func(norm_regret)
+        act = np.random.choice(self.env.tasks, p=ch)
+        #print("Regret : ", self.cumulated_reget)
+
+
+        r = random.random()
+        if(r < self.inertia_factor):
+            logging.debug("INERTIA ! ")
+            return self.precedent_action
+
+        if(self.id == 2):
+            logging.debug('Regret  '+str(self.cumulated_reget))
+            logging.debug('norm_regret  '+str(norm_regret))
+            logging.debug('Distribution  '+str(ch))
+            logging.debug("Acting : "+ str(act))
+        
+        self.precedent_action = act
+        return act
--- core/Simulation.py
+++ core/Simulation.py
@@ -5,7 +5,7 @@ from Environnement import *
 from Simulation import *
 from Player import *
 from utils import *
-
+from Functions import *
 
 logging.basicConfig(level=logging.DEBUG,
                     format='[%(levelname)s] (%(threadName)) : %(message)s',
@@ -20,28 +20,29 @@ logging.basicConfig(level=logging.DEBUG,
 
 
 if(__name__ == "__main__"):
-    env = Environnement(n_tasks = 2,n_players = 2)
-    #env.utility[0][1][2][0] = 1500
-    #env.utility[0][1][2][1] = 1500
-    #env.utility[0][1][2][2] = 1500
+    env = Environnement(n_tasks = 3,n_players = 3)
+    env.utility[0][1][2][0] = 1500
+    env.utility[0][1][2][1] = 1500
+    env.utility[0][1][2][2] = 1500
 
-    env.utility[0][1][0] = 1500
-    env.utility[0][1][1] = 1500
+    #env.utility[0][1][0] = 20
+    #env.utility[0][1][1] = 20
+#
+    P1 = GeneralizedRegretMatchingPlayer(env, 0, inertia_factor = 0.1, fadding_factor = 0.2)
+    P2 = GeneralizedRegretMatchingPlayer(env, 1, inertia_factor = 0.1, fadding_factor = 0.2)
+    P3 = GeneralizedRegretMatchingPlayer(env, 2, inertia_factor = 0.1, fadding_factor = 0.2)
 
-    P1 = RMPlayer(env, 0)
-    P2 = RMPlayer(env, 1)
-    #P3 = FPPlayer(env, 2)
-
-    t = threading.Thread(name="Player "+str(P1.id), target=P1.getWorkerFunction(10,verbose=True))
-    w = threading.Thread(name="Player "+str(P2.id), target=P2.getWorkerFunction(5,verbose=True))
-    #s = threading.Thread(name="Player "+str(P3.id), target=P3.getWorkerFunction(5,verbose=True))
-
-    w.start()
-    t.start()
-    #s.start()
-
-    w.join()
-    t.join()
-    #s.join()
+    for i in range(0,10):
+        print("===")
+        t = threading.Thread(name="Player "+str(P1.id), target=P1.getWorkerFunction(10,verbose=False))
+        w = threading.Thread(name="Player "+str(P2.id), target=P2.getWorkerFunction(10,verbose=False))
+        s = threading.Thread(name="Player "+str(P3.id), target=P3.getWorkerFunction(10,verbose=False))
+        w.start()
+        t.start()
+        s.start()
+        w.join()
+        t.join()
+        s.join()
+        print("===")
 
     print(env.is_EN())
